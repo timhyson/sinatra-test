da1d213de2c6f4702084f72a783cecdbde062df3
Tim Hyson <tjhyson@gmail.com>
2015-09-14 13:52:05 +0100

    index.erb file created

--- sinatra-test/hello.rb
+++ sinatra-test/hello.rb
@@ -1,11 +1,5 @@
 require 'sinatra'
 
 get '/' do
-  "<div style='border: 3px dashed red'>
-  <img src='http://bit.ly/1eze8aE'>
-  </div>"
-end
-
-get '/secret' do
-  "Hello World"
+  erb :index
 end
